6649835e315dbbb2f7fb91f9a592843d3801043f
mohammad abdelfattah <hammodaimad@gmail.com>
2023-06-17 13:18:22 +0300

    first commit

---
+++ String Library/String Library.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string >
+#include <vector>
+#include "clsString.h";
+using namespace std;
+int main(void) {
+    clsString String1;
+    String1.Value = "Mohammad Abdelfattah Wasf";
+    return 0;
+}
